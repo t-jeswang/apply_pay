9e032ba252ce3db4b48a9a269e895e70d2b3bf55
t-jeswang <107151882+t-jeswang@users.noreply.github.com>
2022-06-18 15:01:12 -0700

    Add files via upload

--- adyen_app.py
+++ adyen_app.py
@@ -1,25 +1,78 @@
 
 
+from unittest import result
 from Adyen.util import is_valid_hmac_notification
 from flask import Flask, render_template, send_from_directory, request, abort
-
-from adyen_test import adyen_sessions
+from flask_cors import CORS
+from adyen_test import *
 
 def create_app():
     app = Flask('app')
-
+    CORS(app)
     # Register 404 handler
     # app.register_error_handler(404, page_not_found)
 
     # Routes:
     @app.route('/')
     def home():
+        return render_template('cart.html')
+    @app.route('/cart')
+    def cart():
+        return render_template('cart.html')
+    @app.route('/checkout')
+    def checkout():
         return render_template('index.html')
 
+    @app.route('/payment/<integration>')
+    
+    def payment(integration):
+
+        
+        return render_template('payment_process.html', method=integration, client_key=get_adyen_client_key())
+        
+    @app.route('/api/sessions', methods=['POST'])
+    def sessions():
+
+        host_url = request.host_url 
+        response= adyen_sessions(host_url)
+      
+          
+        return response
+       
+    # @app.route('/payment_process')
+    # # def apple_pay():    # change here
+    # #     applePayConfiguration = { "amount": { "value": 10,
+    # #         "currency": "USD"
+    # #     },
+    # #     "countryCode": "US"
+    # #     }
+    
+
+    @app.route('/redirect', methods=['POST', 'GET'])
+    def redirect():
+
+        return render_template('payment_process.html', method=None, client_key=get_adyen_client_key())
+
+    #     return render_template('apple_pay_form.html')
+    # @app.route('/api/sessions', methods=['POST'])
   
+    @app.route('/result/failed', methods=['GET'])
+    def checkout_failure():
+        return render_template('payment_fail.html')
+
+    @app.route('/result/pending', methods=['GET'])
+    def checkout_pending():
+        return render_template('payment_fail.html')
+
+    @app.route('/result/error', methods=['GET'])
+    def checkout_error():
+        return render_template('payment_fail.html')
+
+     
+
     return app
 
 
 if __name__ == '__main__':
     web_app = create_app()
-    web_app.run()
+    web_app.run(debug=True, port=get_port(), host='0.0.0.0')
--- adyen_test.py
+++ adyen_test.py
@@ -1,11 +1,14 @@
 import Adyen
 import json
-
-
+import uuid
+def get_adyen_client_key():
+    return "test_DT7EVPYLGNDHXI3QUMU63DAQNYIO45DI"
+def get_adyen_hmac_key():
+  return "89347796AC5FDEC4866BEBACDA5D07DF074BCCD102D9A4BE1D5A7F8F6E77FC65"
 def adyen_sessions(host_url):
     
     ady = Adyen.Adyen()
-    ady.payment.client.xapikey = "AQEnhmfuXNWTK0Qc+iSdm2cqqPeUTp8YXMpA6r4K0do+R+AIcN9JO7uMEMFdWw2+5HzctViMSCJMYAc=-5sHWLSXqKKvKQbMhmcmiS9t4sQedfk0pYgSn+GvJqR0=-UI8,wCV2fgft>.32"
+    ady.payment.client.xapikey ="AQEnhmfuXNWTK0Qc+iSdm2cqqPeUTp8YXMpA6r4K0do+R+AIcN9JO7uMEMFdWw2+5HzctViMSCJMYAc=-U5i1IxiMBCMIe5ePGx4YhXR/bmZ2DpZsj7769NUKSYw=-+64Ier)A#{@Ta9,m"
     ady.payment.client.merchant_account = "Microsoft519_000001_TEST"
     ady.payment.client.platform = "test" # change to live for production
     ady.payment.client.hmac = "89347796AC5FDEC4866BEBACDA5D07DF074BCCD102D9A4BE1D5A7F8F6E77FC65"
@@ -13,15 +16,58 @@ def adyen_sessions(host_url):
 
     request = {}
 
-    request['amount'] = {"value": "1000", "currency": "EUR"}
-    # request['reference'] = f"Reference {uuid.uuid4()}"  # provide your unique payment reference
+    request['amount'] = {"value": "100", "currency": "USD"}  
+    request['reference'] = f"Reference {uuid.uuid4()}"  # provide your unique payment reference
+    # universally unique identifier
     # set redirect URL required for some payment methods
     request['returnUrl'] = f"{host_url}/redirect?shopperOrder=myRef"
-    request['countryCode'] = "NL"
+    request['countryCode'] = "US"
 
     result = ady.checkout.sessions(request)
-
+    
     formatted_response = json.dumps((json.loads(result.raw_response)))
+    # print("/sessions response:\n" + formatted_response)
+
+    return formatted_response, result.status_code
+
+def adyen_apple_checkout(host_url):
+    session = json.loads(adyen_sessions(host_url))
+    if session == {}: return {}
+    ady = Adyen.Adyen()
+    ady.payment.client.xapikey = get_adyen_client_key()
+    ady.payment.client.merchant_account = "Microsoft519_000001_TEST"
+    ady.payment.client.platform = "test" # change to live for production
+    ady.payment.client.hmac = "89347796AC5FDEC4866BEBACDA5D07DF074BCCD102D9A4BE1D5A7F8F6E77FC65"
+    request = {}
+    request['reference'] = f"Reference {uuid.uuid4()}"
+
+    request['amount'] = {"value": "10", "currency": "USD"}  
+
+
+    request['returnUrl'] = f"{host_url}/redirect?shopperOrder=myRef"
+    request['countryCode'] = "US"
+
+    request['sdkVersion'] = '5.16.0'
+
+    
+    # request['card'] = {
+    #   'number': '6011000994462780',
+    #   'expiryMonth': '11',
+    #   'expiryYear': '2022',
+    #   'cvc': '111',
+    #   'holderName': 'alice'
+    # }
+    #     request['session'] = {'id':session['id'],
+    #  'sessionData': session['sessionData'] }
+    # request['environment'] = 'test'
+    # request['clientKey'] = get_adyen_client_key()
+   
+      
+    checkout = ady.checkout.payment_session(request)
+    
+
+    print(checkout.status_code)
+    formatted_response = json.dumps((json.loads(checkout.raw_response)))
     print("/sessions response:\n" + formatted_response)
 
-    return formatted_response
+    return formatted_response, checkout.status_code
